489f55650fa5e4a08ca6b42804c5cadec6030578
Emil Nymann <emilnymann96@gmail.com>
2020-03-18 10:34:33 +0100

    Milepæl 2

--- app/src/main/java/com/example/mvvm_demo/MainActivity.java
+++ app/src/main/java/com/example/mvvm_demo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.mvvm_demo;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeText(View view) {
+        TextView textView = findViewById(R.id.text_view);
+        EditText editText = findViewById(R.id.edit_text);
+
+        CharSequence text = editText.getText();
+        textView.setText(text);
     }
 }
